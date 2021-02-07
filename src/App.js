c2c3f9bbd6c5f434f5d7dc4b0014c3b7771e8d64
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-07 19:13:10 +0500

    Signup without the profile picture - firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Signup from "./Signup";
+import Example from "./Navbar";
+import { Col, Container, Row } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Example />
+      <Container
+        className="border border-danger text-center d-flex align-items-center justify-content-center"
+        style={{ minHeight: "90vh" }}
+      >
+        <Row className="">
+          <Col xs="12" md="6" className="border border-danger my-auto">
+            <h1>Welcome</h1>
+          </Col>
+          <Col xs="12" md="6" className="border border-danger">
+            <Signup />
+          </Col>
+        </Row>
+      </Container>
+      {/* <Signup /> */}
+    </>
   );
 }
 
