960842cb88829373be500831b49aef4f96fd80cc
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-21 12:00:09 +0500

    Simple Sign-in

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { fire } from "./firebase";
 
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let history = useHistory();
+
+  const handleSubmit = (e) => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        // var user = userCredential.user;
+        // ...
+
+        console.log("Login Successful");
+
+        // window.location = "/newsfeed";
+        history.push("/newsfeed");
+      })
+      .catch((error) => {
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorMessage);
+      });
+    e.preventDefault();
+  };
+
   return (
     <>
       <img
@@ -11,7 +41,7 @@ const Login = (props) => {
         alt="twitter"
       />
       <h1 className="mb-4">Login to Twitter</h1>
-      <Form className="w-25 mx-auto">
+      <Form className="w-25 mx-auto" onSubmit={handleSubmit}>
         <FormGroup>
           <Label for="exampleEmail">Email</Label>
           <Input
@@ -20,6 +50,7 @@ const Login = (props) => {
             id="exampleEmail"
             placeholder="something@idk.cool"
             required
+            onChange={(e) => setEmail(e.target.value)}
           />
         </FormGroup>
         <FormGroup>
@@ -30,6 +61,7 @@ const Login = (props) => {
             id="examplePassword"
             placeholder="don't tell!"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </FormGroup>
         <Button>Submit</Button>
