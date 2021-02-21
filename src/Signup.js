a2fb455da8df0bc78d0b9c3f3bdd4876815cdb25
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-21 12:03:34 +0500

    Added ability to signup and store data in the database

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
-import fire from "./firebase";
+import { fire } from "./firebase";
 
 const Signup = (props) => {
   const [email, setEmail] = useState("");
@@ -11,8 +21,15 @@ const Signup = (props) => {
   const [contact, setContact] = useState("");
   const [profileImage, setProfileImage] = useState("");
 
-  function handleSubmit(e) {
+  let history = useHistory();
+
+  // useEffect(() => {
+
+  // }, []);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(profileImage.name);
 
     fire
       .auth()
@@ -22,116 +39,134 @@ const Signup = (props) => {
         var user = userCredential.user;
         // ...
 
-        fire.database().ref(user.uid).set({
-          uid: user.uid,
-          email: email,
-          user_name: username,
-          password: password,
-          full_name: fullName,
-          contact: contact,
-          // profile_picture: profileImage,
-        });
-
-        // Get a reference to the storage service, which is used to create references in your storage bucket
         var storage = fire.storage();
 
-        // Create a storage reference from our storage service
         var storageRef = storage.ref();
 
-        // Create a child reference
-        var imagesRef = storageRef.child(user.uid);
-        // imagesRef now points to 'images'
+        var imagesRef = storageRef.child(username);
 
-        // Child references can also take paths delimited by '/'
-        var spaceRef = storageRef.child(imagesRef + "/" + profileImage);
-        // spaceRef now points to "images/space.jpg"
-        // imagesRef still points to "images"
+        imagesRef.put(profileImage).then((snapshot) => {
+          console.log("Uploaded a blob or file!");
+          snapshot.ref.getDownloadURL().then((downloadURL) => {
+            console.log("File available at", downloadURL);
+
+            fire
+              .database()
+              .ref("users/" + user.uid)
+              .set({
+                uid: user.uid,
+                email: email,
+                user_name: username,
+                password: password,
+                full_name: fullName,
+                contact: contact,
+                profile_picture: downloadURL,
+              });
+          });
+        });
+
+        // window.location = "/newsfeed";
+        history.push("/newsfeed");
       })
       .catch((error) => {
-        var errorCode = error.code;
+        // var errorCode = error.code;
         var errorMessage = error.message;
         // ..
         console.log(errorMessage);
       });
-  }
+  };
 
   return (
     <>
-      <img
-        src="https://cdn.worldvectorlogo.com/logos/twitter-5.svg"
-        width="100"
-        height="100"
-        alt="twitter"
-      />
-      <h1 className="mb-4">Signup to Twitter</h1>
-      <Form className="w-75 mx-auto" onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            id="exampleEmail"
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="something@idk.cool"
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleUsername">Username</Label>
-          <Input
-            type="text"
-            name="text"
-            id="exampleUsername"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="don't tell!"
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFullname">Full Name</Label>
-          <Input
-            type="text"
-            name="text"
-            onChange={(e) => setFullName(e.target.value)}
-            id="exampleFullname"
-            placeholder="Full Name"
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleNumber">Contact No</Label>
-          <Input
-            onChange={(e) => setContact(e.target.value)}
-            type="number"
-            name="Number"
-            id="exampleNumber"
-            placeholder="Contact No"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">Profile Image</Label>
-          <Input
-            type="file"
-            name="file"
-            id="exampleFile"
-            onChange={(e) => setProfileImage(e.target.files[0])}
-          />
-        </FormGroup>
-        <Button color="primary" block>
-          Submit
-        </Button>
-      </Form>
+      <Container
+        className="border border-danger text-center d-flex align-items-center justify-content-center"
+        style={{ minHeight: "90vh" }}
+      >
+        <Row>
+          <Col xs="12" md="6" className="border border-danger my-auto">
+            <h1>Welcome</h1>
+          </Col>
+          <Col xs="12" md="6" className="border border-danger">
+            {/* <Signup /> */}
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/twitter-5.svg"
+              width="100"
+              height="100"
+              alt="twitter"
+            />
+            <h1 className="mb-4">Signup to Twitter</h1>
+            <Form className="w-75 mx-auto" onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="exampleEmail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="something@idk.cool"
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleUsername">Username</Label>
+                <Input
+                  type="text"
+                  name="text"
+                  id="exampleUsername"
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="examplePassword"
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="don't tell!"
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleFullname">Full Name</Label>
+                <Input
+                  type="text"
+                  name="text"
+                  onChange={(e) => setFullName(e.target.value)}
+                  id="exampleFullname"
+                  placeholder="Full Name"
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleNumber">Contact No</Label>
+                <Input
+                  onChange={(e) => setContact(e.target.value)}
+                  type="number"
+                  name="Number"
+                  id="exampleNumber"
+                  placeholder="Contact No"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleFile">Profile Image</Label>
+                <Input
+                  type="file"
+                  name="file"
+                  id="exampleFile"
+                  onChange={(e) => setProfileImage(e.target.files[0])}
+                />
+                {/* {onFileChange()} */}
+              </FormGroup>
+              <Button color="primary" block>
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
