4bc34e7a68d3070028be4fa6ca89f80838ab869c
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-21 14:08:38 +0500

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import Signup from "./Signup";
 import Example from "./Navbar";
 // import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Login from "./Login";
 import NewsFeed from "./components/Newsfeed";
+import { useEffect, useState } from "react";
+import { fire } from "./firebase";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fire.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.
+        setLoggedIn(!loggedIn);
+      } else {
+        // No user is signed in.
+      }
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Example />
       <Switch>
-        <Route exact path="/">
+        {/* <Route exact path="/">
           <Signup />
+        </Route> */}
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/newsfeed" /> : <Signup />}
         </Route>
-        <Route path="/login">
+        {/* <Route path="/login">
           <Login />
+        </Route> */}
+        <Route path="/login">
+          {loggedIn ? <Redirect to="/newsfeed" /> : <Login />}
         </Route>
         <Route path="/newsfeed">
           <NewsFeed />
