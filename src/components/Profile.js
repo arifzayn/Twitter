fa0919fa7d2b0412431d788bcd0f78099242e015
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-24 13:20:36 +0500

    user can update profile and view tweets

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,6 +17,7 @@ import {
   NavItem,
   Nav,
   NavLink,
+  CardImg,
 } from "reactstrap";
 
 import classnames from "classnames";
@@ -27,6 +28,7 @@ const Profile = () => {
   const [name, setName] = useState("");
   const [contact, setContact] = useState("");
   const [status, setStatus] = useState("");
+  const [data, setData] = useState("");
 
   const toggle = (tab) => {
     if (activeTab !== tab) setActiveTab(tab);
@@ -36,6 +38,22 @@ const Profile = () => {
     fire.auth().onAuthStateChanged(function (user) {
       if (user) {
         // User is signed in.
+
+        fire
+          .database()
+          .ref("tweets/" + user.uid)
+          .once("value", (snapshot) => {
+            setData(snapshot.val());
+            // Object.values(data).forEach((ele) => {
+            //   console.log(ele);
+            // });
+            // snapshot.forEach((childSnapshot) => {
+            //   // var childKey = childSnapshot.key ;
+            //   // var childData = childSnapshot.val();
+            //   // ...
+            // });
+          });
+
         fire
           .database()
           .ref("users/" + user.uid)
@@ -60,16 +78,25 @@ const Profile = () => {
     fire.auth().onAuthStateChanged(function (user) {
       if (user) {
         // User is signed in.
+
         fire
           .database()
           .ref("users/" + user.uid)
-          .set({
-            // name: document.getElementById("editName").value,
-            // description: document.getElementById("editDescription").value,
-            // status: document.getElementById("editStatus").value,
-            // userId: uid,
-            contact: contact,
-          });
+          .update(
+            {
+              full_name: name,
+              contact: contact,
+              status: status,
+            },
+            (error) => {
+              if (error) {
+                // The write failed...
+              } else {
+                // Data saved successfully!
+                console.log("Profile Updated!");
+              }
+            }
+          );
       } else {
         // No user is signed in.
       }
@@ -77,128 +104,146 @@ const Profile = () => {
     console.log("Task Edited!");
   };
 
-  const handleInputChange = (e) => {
-    // const target = e.target;
-    // const value = target.type === "checkbox" ? target.checked : target.value;
-    // const name = target.name;
-
-    setName(e.target.contact);
-    // setContact(snapshot.val().contact);
-    // setStatus(snapshot.val().status);
-  };
-
   return (
-    <Container>
-      <h1 className="text-center display-1">Profile</h1>
-      <Row className=" mt-4">
-        <Col xs="12" md="6" className="border border-danger">
-          <Form className="w-75 mx-auto" onSubmit={handleSubmit}>
-            <FormGroup row>
-              <Label for="exampleEmail" sm={2}>
-                Name
-              </Label>
-              <Col sm={10}>
-                <Input
-                  type="text"
-                  id="exampleEmail"
-                  value={name}
-                  onChange={handleInputChange}
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="examplePassword" sm={2}>
-                Contact
-              </Label>
-              <Col sm={10}>
-                <Input
-                  type="number"
-                  id="examplePassword"
-                  name="contact"
-                  value={contact}
-                  onChange={handleInputChange}
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="exampleEmail" sm={2}>
-                Status
-              </Label>
-              <Col sm={10}>
-                <Input
-                  type="text"
-                  id="exampleEmail"
-                  value={status}
-                  onChange={handleInputChange}
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="exampleFile" sm={2}>
-                Profile Picture
-              </Label>
-              <Col sm={10}>
-                <Input type="file" name="file" id="exampleFile" />
-                <FormText color="muted">Choose a profile picture</FormText>
-              </Col>
-            </FormGroup>
-            <FormGroup check row>
-              <Col sm={{ size: 10, offset: 2 }}>
-                <Button>Update</Button>
-              </Col>
-            </FormGroup>
-          </Form>
-        </Col>
-
-        <Col xs="12" md="6" className="border border-danger">
-          <div>
-            <Nav tabs>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: activeTab === "1" })}
-                  onClick={() => {
-                    toggle("1");
-                  }}
-                >
-                  Tweets
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: activeTab === "2" })}
-                  onClick={() => {
-                    toggle("2");
-                  }}
-                >
-                  Likes
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: activeTab === "3" })}
-                  onClick={() => {
-                    toggle("3");
-                  }}
-                >
-                  Followers/Followings
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeTab={activeTab}>
-              <TabPane tabId="1">
-                <Col sm="12" className="mt-4">
-                  <Card body>
-                    <CardTitle>Special Title Treatment</CardTitle>
-                    <CardText>
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </CardText>
-                    {/* <Button>Go somewhere</Button> */}
-                  </Card>
+    <div
+      style={{
+        // backgroundImage: 'url("")',
+        // backgroundSize: "cover",
+        // backgroundRepeat: "no-repeat",
+        height: "93vh",
+        // background: #74ebd5;  /* fallback for old browsers */
+        // background: -webkit-linear-gradient(to right, #ACB6E5, #74ebd5);  /* Chrome 10-25, Safari 5.1-6 */
+        // background:
+        // "linear-gradient(to right, #ACB6E5, #74ebd5)" /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */,
+      }}
+    >
+      <Container>
+        <h1 className="text-center display-1">Profile</h1>
+        <Row className="mt-4">
+          <Col xs="12" md="6" className="border border-danger mb-4">
+            <Form className="w-75 mx-auto" onSubmit={handleSubmit}>
+              <FormGroup row>
+                <Label for="exampleName" sm={2}>
+                  Name
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="text"
+                    id="exampleName"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </Col>
-              </TabPane>
-              <TabPane tabId="2">
-                {/* <Row>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="exampleContact" sm={2}>
+                  Contact
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="number"
+                    id="exampleContact"
+                    name="contact"
+                    value={contact}
+                    onChange={(e) => setContact(e.target.value)}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="exampleStatus" sm={2}>
+                  Status
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="text"
+                    id="exampleStatus"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="exampleFile" sm={2}>
+                  Profile Picture
+                </Label>
+                <Col sm={10}>
+                  <Input type="file" name="file" id="exampleFile" />
+                  <FormText color="muted">Choose a profile picture</FormText>
+                </Col>
+              </FormGroup>
+              <FormGroup check row>
+                <Col sm={{ size: 10, offset: 2 }}>
+                  <Button>Update</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Col>
+
+          <Col xs="12" md="6" className="border border-danger">
+            <div>
+              <Nav tabs>
+                <NavItem>
+                  <NavLink
+                    className={classnames({ active: activeTab === "1" })}
+                    onClick={() => {
+                      toggle("1");
+                    }}
+                  >
+                    Tweets
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={classnames({ active: activeTab === "2" })}
+                    onClick={() => {
+                      toggle("2");
+                    }}
+                  >
+                    Likes
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={classnames({ active: activeTab === "3" })}
+                    onClick={() => {
+                      toggle("3");
+                    }}
+                  >
+                    Followers/Followings
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                  <Col sm="12" className="mt-4">
+                    {Object.values(data).map((d, index) => {
+                      return (
+                        <div className="d-flex justify-content-center">
+                          <Card
+                            key={index}
+                            className="mb-4"
+                            style={{
+                              width: "18rem",
+                            }}
+                          >
+                            <CardImg
+                              top
+                              src={d.tweet_image}
+                              alt="tweet_image"
+                            />
+                            <Card body>
+                              <CardTitle>{d.id}</CardTitle>
+                              <CardText>{d.tweet_text}</CardText>
+                              {/* <Button>Go somewhere</Button> */}
+                            </Card>
+                          </Card>
+                        </div>
+                      );
+                    })}
+                  </Col>
+                </TabPane>
+                <TabPane tabId="2">
+                  {/* <Row>
                   <Col sm="6">
                     <Card body>
                       <CardTitle>Special Title Treatment</CardTitle>
@@ -220,13 +265,14 @@ const Profile = () => {
                     </Card>
                   </Col>
                 </Row> */}
-              </TabPane>
-              <TabPane tabId="3"></TabPane>
-            </TabContent>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+                </TabPane>
+                <TabPane tabId="3"></TabPane>
+              </TabContent>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
