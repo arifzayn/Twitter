82074e8c50ab57389d736b541693a83bf9b80f47
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-27 17:28:06 +0500

    added react-awesome-button

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,21 +14,22 @@ import {
 } from "reactstrap";
 import { NavLink as RRNavLink } from "react-router-dom";
 import { fire } from "./firebase";
+import "./App.css";
 
-const Example = () => {
+import { AwesomeButtonSocial } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+
+const Navigationbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   const [users, setUser] = useState(null);
 
-  // let history = useHistory();
-
   fire.auth().onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
       setUser(user);
-      // console.log(user);
     } else {
       // No user is signed in.
     }
@@ -42,7 +43,6 @@ const Example = () => {
         // Sign-out successful.
         console.log("Sign-out successful.");
         window.location = "/";
-        // history.push("/");
       })
       .catch((error) => {
         // An error happened.
@@ -51,13 +51,22 @@ const Example = () => {
   };
 
   return (
-    <Navbar color="light" light expand="md" sticky="top">
+    <Navbar
+      color="light"
+      light
+      expand="md"
+      sticky="top"
+      className="simg"
+      // style={{
+      //   background: "linear-gradient(to right, #ACB6E5, #74ebd5)",
+      // }}
+    >
       <NavbarBrand href="/">Twitter</NavbarBrand>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
           {users ? (
-            <UncontrolledDropdown nav inNavbar>
+            <UncontrolledDropdown nav inNavbar className="mr-4">
               <DropdownToggle nav caret>
                 {users.email}
               </DropdownToggle>
@@ -81,14 +90,26 @@ const Example = () => {
             </UncontrolledDropdown>
           ) : (
             <NavItem>
-              <NavLink tag={RRNavLink} to="/login" onClick={toggle}>
+              <NavLink
+                tag={RRNavLink}
+                to="/login"
+                onClick={toggle}
+                className="mr-4"
+              >
                 Login
               </NavLink>
             </NavItem>
           )}
 
           <NavItem>
-            <NavLink href="https://github.com/arifzayn">GitHub</NavLink>
+            {/* <NavLink> */}
+            <AwesomeButtonSocial
+              type="github"
+              target="_blank"
+              href="https://github.com/arifzayn"
+              size="medium"
+            ></AwesomeButtonSocial>
+            {/* </NavLink> */}
           </NavItem>
         </Nav>
       </Collapse>
@@ -96,4 +117,4 @@ const Example = () => {
   );
 };
 
-export default Example;
+export default Navigationbar;
