8ef53b18e222be11e56b701d2cc116f5672f1c13
arifzayn <60439783+arifzayn@users.noreply.github.com>
2021-02-27 17:29:34 +0500

    working feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,52 +5,33 @@ import {
   CardText,
   CardBody,
   CardTitle,
-  // CardSubtitle,
   Button,
 } from "reactstrap";
 
 const Feed = ({ tweets }) => {
-  // console.log(tweets);
-  // console.log(Object.values(tweets));
-
-  Object.values(tweets).forEach((ele) => {
-    // console.log(ele);
-    Object.values(ele).forEach((el) => {
-      console.log(el);
-    });
-  });
-
   return Object.values(tweets).map((d, i) => {
-    const [data] = Object.values(d);
-
-    // console.log(data);
-
-    // data.forEach((el) => {
-    // console.log(el);
-    // });
-
-    return (
-      <div key={i} className="mb-2">
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src={data.tweet_image}
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle tag="h5">From: {data.id}</CardTitle>
-            {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
-              Card subtitle
-            </CardSubtitle> */}
-            <CardText>{data.tweet_text}</CardText>
-            <Button color="link" size="sm">
-              Like
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-    );
+    const data = Object.values(d);
+    return Object.values(data).map((element, index) => {
+      return (
+        <div key={index} className="d-flex justify-content-center">
+          <Card className="mb-2">
+            <CardImg
+              top
+              width="100%"
+              src={element.tweet_image}
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">From: {element.id}</CardTitle>
+              <CardText>{element.tweet_text}</CardText>
+              <Button color="link" size="sm" outline>
+                Like
+              </Button>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
   });
 };
 
